dded65634c3b5e9f95210a0f7d7dee62d0df6e97
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-26 03:16:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,5 @@
 # Write your code here!
 def game_hash 
+  {:home => {:team_name => "Brookyln Nets",:colors => ["Black","White"],:players => [{:player_name => "Alan Anderson"},{:number => 0},{:shoe => 16},{:points => 22},{:rebounds => 12},{:assists => 12},{:steals => 3}, {:blocks => 1},{:slam_dunk => 1}]},
+   :away => {:team_name => "Charlotte Hornets",:colors => ["Turquoise","Purple"],:players}
+  }
